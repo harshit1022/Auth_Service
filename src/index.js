4a4f3b79c2bc3987d3e138cc4299053bf1e9a912
Harshit Patel <harshitpatel1022@gmail.com>
2023-10-10 13:31:53 +0530

    Added JWT Generation and Verification Logic

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
-//const UserRepository = require('./repository/user-repository');
+const UserService = require('./services/user-service');
 
 const app = express();
 
@@ -17,10 +17,22 @@ const prepareAndStartServer = () => {
   app.listen(PORT, async () => {
     console.log(`Server started on Port: ${PORT}`);
 
-    // const repo = new UserRepository();
-
-    // const response = await repo.getById(2);
+    //---------TESTING TOKEN GENERATION AND VERIFICATION ON CONSOLE---------
+    //const userService = new UserService();
+    
+    // const result = userService.createToken({email: "harshit@admin.com", password: '123HP'});
+    // console.log(result);
+    // Output: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhhcnNoaXRAYWRtaW4uY29tIiwicGFzc3dvcmQiOiIxMjNIUCIsImlhdCI6MTY5NjkyNDQ3NCwiZXhwIjoxNjk2OTMxNjc0fQ.SJ9GJ7HjsKFxUHI2DJJfOKYtyTI6vdE4-Ks2L-5SwnU'
+    
+    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhhcnNoaXRAYWRtaW4uY29tIiwicGFzc3dvcmQiOiIxMjNIUCIsImlhdCI6MTY5NjkyNDQ3NCwiZXhwIjoxNjk2OTMxNjc0fQ.SJ9GJ7HjsKFxUHI2DJJfOKYtyTI6vdE4-Ks2L-5SwnU';
+    // const response = userService.verifyToken(token);
     // console.log(response);
+    // Output:   {
+              //   email: 'harshit@admin.com',
+              //   password: '123HP',
+              //   iat: 1696924474,
+              //   exp: 1696931674
+              // }
   });
 }
 
